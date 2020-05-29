9d5e7ba9f17f06124127442f1312cc4067db61c0
rsingh28 <rashmeet.singh28@gmail.com>
2020-05-29 14:19:40 -0400

    Initial Commit

---
+++ FirstARApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstARApp
+//
+//  Created by Rashmeet Singh on 2020-05-29.
+//  Copyright © 2020 Rashmeet Singh. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
